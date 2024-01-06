429b10fed3999fc988604526a1cb28f57a627059
terachan <tadahisa.terao777@gmail.com>
2024-01-06 21:58:37 +0900

    20240106

--- wage_streamlit.py
+++ wage_streamlit.py
@@ -4,69 +4,70 @@ import streamlit as st
 import pydeck as pdk
 import plotly.express as px
 
-st.title('日本の賃金データのダッシュボード')
+st.title('日本の医療オープンデータ解析DB')
+st.text('本結果は厚生労働省(医療費の地域差分析)のオープンデータを加工して作成しております。')
 
 df_jp_ind = pd.read_csv('./csv_data/雇用_医療福祉_一人当たり賃金_全国_全産業.csv',encoding = 'shift_jis')
 df_jp_category = pd.read_csv('./csv_data/雇用_医療福祉_一人当たり賃金_全国_大分類.csv',encoding = 'shift_jis')
 df_pref_ind = pd.read_csv('./csv_data/雇用_医療福祉_一人当たり賃金_都道府県_全産業.csv',encoding = 'shift_jis')
 
-st.header('2019年：一人当たりの平均賃金ヒートマップ')
+r4_5_all = []
+r4_5_all = pd.read_csv('./csv_data/r4-5all.csv')
 
-jp_lat_lon = pd.read_csv('./pref_lat_lon.csv')
-jp_lat_lon = jp_lat_lon.rename(columns={'pref_name':'都道府県名'})
-
-df_pref_map = df_pref_ind[(df_pref_ind['年齢'] == '年齢計') & (df_pref_ind['集計年'] == 2019)]
-df_pref_map = pd.merge(df_pref_map, jp_lat_lon, on='都道府県名')
-df_pref_map['一人当たり賃金(相対比)'] = ((df_pref_map['一人当たり賃金（万円）']- df_pref_map['一人当たり賃金（万円）'].min())/(df_pref_map['一人当たり賃金（万円）'].max()-df_pref_map['一人当たり賃金（万円）'].min()))
+disease_list =pd.read_csv('./csv_data/厚労省疾患名リスト.csv')
+st.sidebar.header('【厚労省定義の疾患分類】')
+st.sidebar.text(disease_list)
 
 
-view = pdk.ViewState(
-    longitude=139.691648,
-    latitude=35.689185,
-    zoom=4,
-    pitch=40.5,
-)
+st.header('■疾患別一人当たり医療費(万円/年)(2022年)')
 
-layer = pdk.Layer(
-    "HeatmapLayer",
-    data=df_pref_map,
-    opacity=0.4,
-    get_position=["lon", "lat"],
-    threshold=0.3,
-    get_weight = '一人当たり賃金（相対値）'
-)
-
-layer_map = pdk.Deck(
-    layers=layer,
-    initial_view_state=view,
-)
-
-st.pydeck_chart(layer_map)
-
-show_df = st.checkbox('Show DataFrame')
-if show_df == True:
-    st.write(df_pref_map)
-
-
-st.header('集計年別の一人当たり賃金（万円）の推移')
-
-df_ts_mean = df_jp_ind[(df_jp_ind["年齢"] == "年齢計")]
-df_ts_mean = df_ts_mean.rename(columns={'一人当たり賃金（万円）':'全国_一人当たり賃金（万円）'})
-
-df_pref_mean = df_pref_ind[(df_pref_ind["年齢"] == "年齢計")]
-pref_list = df_pref_mean['都道府県名'].unique()
-
+pref_list = r4_5_all.columns.values[1:]
 option_pref = st.selectbox(
     '都道府県名',
-    (pref_list)
-)
+    (pref_list))
 
-df_pref_mean = df_pref_mean[df_pref_mean['都道府県名'] == option_pref]
+#max_x = r4_5_all[option_pref].max()
 
-df_mean_line = pd.merge(df_ts_mean, df_pref_mean, on='集計年')
-df_mean_line = df_mean_line[['集計年', '全国_一人当たり賃金（万円）', '一人当たり賃金（万円）']]
-df_mean_line = df_mean_line.set_index('集計年')
-st.line_chart(df_mean_line)
+fig = px.bar(r4_5_all,
+            x=option_pref,
+            y="疾患名",
+            color="疾患名",
+            range_x=[0,100000],
+            orientation='h',
+            width=800,
+            height=600)
+st.plotly_chart(fig)
 
-st.text('出典：RESAS（地域経済分析システム）')
-st.text('本結果はRESAS（地域経済分析システム）を加工して作成')
+st.text('出典：厚生労働省(医療費の地域差分析)')
+
+#r4_5_graph = r4_5_graph.astype({'全国平均':float,'東京都':float})
+#r4_5_graph.set_index('疾患名')
+#st.line_chart(r4_5_graph)
+
+#pref_list = r4_5_all['都道府県名']
+
+#option_pref = st.selectbox(
+#    '都道府県',
+#    (pref_list))
+
+
+#option_multi = st.multiselect(
+#    '都道府県を選択して下さい(複数可)',
+#    pref_list,
+#    ['全国平均','東京都']
+#)
+
+#option_multi
+
+
+#r4_5_all = r4_5_all.rename(columns={'全疾患平均': '全疾患合計'})
+#x_axis = r4_5_all.iloc[0:0,1:]
+#y_data = r4_5_all[(r4_5_all["都道府県名"] == option_pref)]
+#y_data.iloc[0:0,1:] = y_data.iloc[0:0,1:].astype(float)
+#y_data2 = y_data.transpose()
+
+
+#st.bar_chart(y_data2)
+#st.write(
+#    px.line(x_axis, y_data, title="test")
+#)
