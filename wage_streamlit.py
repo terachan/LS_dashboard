87133002778654b281cc0e9002d357b5dbc36055
terachan <tadahisa.terao777@gmail.com>
2024-01-12 21:44:14 +0900

    202401122201

--- wage_streamlit.py
+++ wage_streamlit.py
@@ -288,8 +288,8 @@ with tab2:
     st.header('生活習慣との相関')
     st.write('各都道府県ごとのデータ(サマリータブ①で選択した疾患分類における一人当たり医療費(Y軸) X 各種生活習慣(飲酒量/喫煙率等)(X軸))をマッピングします。右肩上がりにマッピングされているほど、その生活習慣がその疾患の医療費に影響している(=相関が高い)ことを示しています。')
 
-    sakeImg = Image.open('./pics/sake0.png')
-    smokeImg = Image.open('./pics/smoke0.png')
+    sakeImg = Image.open('./pics/sake0.PNG')
+    smokeImg = Image.open('./pics/smoke0.PNG')
 
     habits = pd.read_csv('./csv_data/habits.csv')
     correl = pd.merge(r4_5, habits, on='都道府県名')
